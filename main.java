1f7146cb1694aa7da113a9193aa30f92b6b97e7a
pl1m <69647369+L1ucas@users.noreply.github.com>
2021-12-02 13:24:00 -0300

    Update main.java

--- main.java
+++ main.java
@@ -50,6 +50,22 @@ public class trabalho2 {
 			System.out.println("- Converse com amigos. Fale com eles sobre coisas que você gosta.");
 			System.out.println("- Cuide bem do seu corpo. O exercício ajuda a liberar sua ansiedade");
 			System.out.println("- Se algo em específico te deixa ansioso, algo chamado gatilho, tente respirar fundo e fale com alguém para te ajudar a lidar com esse gatilho.");
+			break;
+		case 3:
+			System.out.println("Certo, em uma escala de 1-10, como você se sente negativamente com procrastinação? ");
+			input2 = obter.nextInt();
+			System.out.println("Por quanto tempo isso acontece? ");
+			input3 = obter.next();
+			
+			System.out.println("");
+			System.out.println("Procrastinação é algo normal, não é preguiça e acontece com todo mundo, talvez você esteja com medo de terminar uma tarefa importante ou sem interesse, é totalmente normal.");
+			System.out.println("Mas aqui algumas dicas para te ajudarem:");
+			System.out.println("1 - Se convença que você pode fazer isso. Você pode vencer a procrastinação");
+			System.out.println("2 - Divida uma tarefa grande em tarefas simples. Por exemplo, ao invés de dizer \"Vou fazer esse trabalho da escola em até 3 semanas\" fale \"Vou escolher 3 pontos principais, depois vou escolher uma pessoa ou um país para esse trabalho\"");
+			System.out.println("3 - Não passe muito tempo planejando. Não tente passar 1 hora decidindo o que é mais importante pra hoje, passe 1 hora realmente fazendo essas coisas importantes");
+			System.out.println("4 - Não passe muito tempo em uma tarefa, faça uma coisa por vez em pequenos intervalos de tempo, como 15 minutos ao invés de 2 horas");
+			System.out.println("5 - Não deixe sua mente de carregar pra outras tarefas menos importantes, termine o que é importante *agora*");
+			System.out.println("6 - Não lute contra as distrações, *elimine* as distrações. Se o YouTube é uma distração, tire ele do celular. Se seu celular é uma distração, remova ele da sua mesa e do seu quarto");
 			
 		}
 		
